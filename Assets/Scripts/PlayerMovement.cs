5ff7d5fe9e4cd55d1eba210ce6b11a460b2ccb6c
Justin Hamilton <justinanthonyhamilton@gmail.com>
2015-01-23 21:06:31 -0800

    Starting movement and instrument stuff

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+  private Rigidbody2D body;
+  
+	public float walkSpeed = 10f;
+
+  void Awake() {
+    body = GetComponent<Rigidbody2D>();
+  }
+
+  void Update() {
+    int normalizedHorizontal = 0;
+    if (Input.GetButton("Left")) {
+      normalizedHorizontal -= 1;
+    }
+
+    if (Input.GetButton("Right")) {
+      normalizedHorizontal += 1;
+    }
+
+    body.AddForce(new Vector2(walkSpeed * normalizedHorizontal, 0));
+  }
+
+
+}
