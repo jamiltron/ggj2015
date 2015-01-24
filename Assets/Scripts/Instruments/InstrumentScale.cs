abe690bc425467edfd0e624e426906c1146e9cb7
Walt Destler <walt@waltdestler.com>
2015-01-24 12:42:05 -0800

    Can configure scale per-key.

--- Assets/Scripts/Instruments/InstrumentScale.cs
+++ Assets/Scripts/Instruments/InstrumentScale.cs
@@ -2,11 +2,12 @@
 
 public class InstrumentScale : MonoBehaviour
 {
-	public Vector3 Scale = new Vector3(2, 2, 2);
+	public Vector3[] Scales;
 	public float ScaleTime = 1;
 	public AnimationCurve ScaleCurve = AnimationCurve.Linear(0, 1, 1, 1);
 
 	private Vector3 _baseScale;
+	private Vector3 _targetScale;
 	private float _lastPress = -1000;
 
 	public void Awake()
@@ -17,13 +18,14 @@ public class InstrumentScale : MonoBehaviour
 	public void OnKeyPress(int keyIndex)
 	{
 		_lastPress = Time.time;
+		Vector3 s = Scales[keyIndex%Scales.Length];
+		_targetScale = new Vector3(_baseScale.x * s.x, _baseScale.y * s.y, _baseScale.z * s.z);
 	}
 
 	public void Update()
 	{
 		float t = Time.time - _lastPress;
 		t = ScaleCurve.Evaluate(t / ScaleTime);
-		Vector3 targetScale = new Vector3(_baseScale.x*Scale.x, _baseScale.y*Scale.y, _baseScale.z*Scale.z);
-		transform.localScale = Vector3.Lerp(_baseScale, targetScale, t);
+		transform.localScale = Vector3.Lerp(_baseScale, _targetScale, t);
 	}
 }
