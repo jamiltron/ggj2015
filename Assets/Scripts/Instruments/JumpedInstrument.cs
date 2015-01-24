20bf8cd56f5351bda04efaa04cd23e42e74cd120
Justin Hamilton <justinanthonyhamilton@gmail.com>
2015-01-24 12:55:47 -0800

    Adding JumpedInstrument

---
+++ Assets/Scripts/Instruments/JumpedInstrument.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class JumpedInstrument: RangedPlayable {
+  
+  private List<GameObject> playersInRange;
+  
+  override public List<GameObject> GetPlayers() {
+    return playersInRange;
+  }
+  
+  void Awake() {
+    playersInRange = new List<GameObject>();
+  }
+  
+  void Update() {
+    foreach (var player in playersInRange) {
+      NetworkView playerView = player.GetComponent<NetworkView>();
+      if (playerView.isMine) {
+        PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+        if (playerMovement.collisionState.becameGroundedThisFrame) {
+          networkView.RPC("SendOnKeyPress", RPCMode.All, 0);
+        }
+      }
+    }
+  }
+  
+  [RPC]
+  void SendOnKeyPress(int keyIndex) {
+    SendMessage("OnKeyPress", keyIndex, SendMessageOptions.DontRequireReceiver);
+  }
+  
+  void OnTriggerEnter2D(Collider2D other) {
+    if (other.gameObject.tag == "Player") {
+      playersInRange.Add(other.gameObject);
+    }
+  }
+  
+  void OnTriggerExit2D(Collider2D other) {
+    if (other.gameObject.tag == "Player") {
+      playersInRange.Remove(other.gameObject);
+    }
+  }
+}
